211827a63bbddb313695e13d6ef152a90358211f
orthoros.san <orthoros.san@gmail.com>
2019-01-17 12:35:14 +0300

    added new class

---
+++ RangeClass/RangeClass/Range.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeClass
+{
+    public class Range
+    {
+        public int From
+        {
+            get;
+            set;
+        }
+        public int To
+        {
+            get;
+            set;
+        }
+
+        public int GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(int yourNumber)
+        {
+            return (yourNumber >= From && yourNumber <= To);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("{0}, {1}", From, To);
+        }
+    }
+}
---
+++ RangeClass/RangeClass/RangeProject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeClass
+{
+    class RangeProject
+    {
+        static void Main(string[] args)
+        {
+            Range range = new Range();
+            range.From = 7;
+            range.To = 19;
+
+            Console.WriteLine("Есть диапазон чисел, длиной {0}. Попробуйте ввести число, попадающее в этот диапазон", range.GetLength());
+
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+            while (!range.IsInside(yourNumber))
+            {
+                if (!range.IsInside(yourNumber))
+                {
+                    Console.WriteLine("Попробуйте еще раз");
+                    yourNumber = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Это число попадает в диапазон {0}-{1}", range.From, range.To);
+            range.Print();
+        }
+    }
+}
