a35f886c067e9f2951ea59f19aa0a7666b8bdad7
orthoros.san <orthoros.san@gmail.com>
2019-04-29 14:15:26 +0300

    added changes

--- RangeClass/ShapesProject/Program.cs
+++ RangeClass/ShapesProject/Program.cs
@@ -12,25 +12,26 @@ namespace ShapesProject
         {
             var areaComparer = new AreaComparer();
             Array.Sort(shapesArray, areaComparer);
-            return shapesArray[0].ToString();
+            return shapesArray[shapesArray.Length - 1].ToString();
         }
 
         public static string GetNextToMaxPerimeterShape(IShape[] shapesArray)
         {
             var perimeterComparer = new PerimeterComparer();
             Array.Sort(shapesArray, perimeterComparer);
-            return shapesArray[1].ToString();
+            return shapesArray[shapesArray.Length - 2].ToString();
         }
 
         static void Main(string[] args)
         {
-            IShape[] shapesArray = new IShape[] { new Circle(4), new Rectangle(2, 4), new Triangle(1, 4, 2, -2, 4, 3), new Square(6), new Circle(3), new Rectangle(3, 2), new Triangle(-1, 4, 2, -2, 4, 2), new Square(8) };
+            IShape[] shapesArray = new IShape[] { new Circle(4), new Rectangle(2, 4), new Triangle(1, 4, 2, -2, 4, 3), new Square(6),
+                                                  new Circle(3), new Rectangle(3, 2), new Triangle(-1, 4, 2, -2, 4, 2), new Square(8) };
 
-            Console.WriteLine("Max area shape: {0}",GetMaxAreaShape(shapesArray));
+            Console.WriteLine("Max area shape: {0}", GetMaxAreaShape(shapesArray));
 
             Console.WriteLine();
 
-            Console.WriteLine("Previous to max perimeter shape: {0}",GetNextToMaxPerimeterShape(shapesArray));
+            Console.WriteLine("Previous to max perimeter shape: {0}", GetNextToMaxPerimeterShape(shapesArray));
         }
     }
 }
