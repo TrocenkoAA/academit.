8c256a17575fc5e6a1c133b172d94ee8c1c83a5f
orthoros.san <orthoros.san@gmail.com>
2019-07-06 00:03:31 +0300

    add "list" task

---
+++ RangeClass/List/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private ListItem<T> head;
+        private ListItem<T> tail;
+        private int count;
+
+        public void Add(T data)
+        {
+            ListItem<T> listItem = new ListItem<T>(data);
+
+            if (head == null)
+            {
+                head = listItem;
+            }
+            else
+            {
+                tail.Next = listItem;
+            }
+
+            tail = listItem;
+            count++;
+        }
+
+        public void RemoveItem(int index)
+        {
+            int indexCount = 0;
+            for (ListItem<T> i = head; index == indexCount; i = i.Next, indexCount++)
+            {
+
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            ListItem<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+        //методы
+    }
+}
---
+++ RangeClass/List/ListItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class ListItem<T>
+    {
+        public ListItem(T data)
+        {
+            Data = data;
+        }
+
+        public T Data
+        {
+            get;
+            set;
+        }
+
+        public ListItem<T> Next
+        {
+            get;
+            set;
+        }
+
+    }
+}
